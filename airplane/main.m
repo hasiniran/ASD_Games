2ded664dd4a38a0aab6a2b44ef4eb7a1fe7a6811
Hasini Yatawatte <Randy@hasinis-air.dhcp.nd.edu>
2014-10-22 12:42:52 -0400

    Initial Commit

---
+++ airplane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  airplane
+//
+//  Created by Hasini Yatawatte on 10/22/14.
+//  Copyright (c) 2014 Hasini Yatawatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ airplaneTests/airplaneTests.m
@@ -0,0 +1,34 @@
+//
+//  airplaneTests.m
+//  airplaneTests
+//
+//  Created by Hasini Yatawatte on 10/22/14.
+//  Copyright (c) 2014 Hasini Yatawatte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface airplaneTests : XCTestCase
+
+@end
+
+@implementation airplaneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
