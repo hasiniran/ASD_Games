c62a01cb0725d012d5c3fb2a34ef34bf43d14829
mperez14 <mperez14@nd.edu>
2015-02-04 22:52:34 -0500

    Added object (Plane for now) with physics body. Looks like object is moving with scrolling background.

--- Level1.m
+++ Level1.m
@@ -9,53 +9,80 @@
 #import "Level1.h"
 //LEVEL1
 @implementation Level1{
-    SKNode *_train;
+    SKSpriteNode *_train;
     SKNode *_bgLayer;
     SKNode *_HUDLayer;
     SKNode *_gameLayer;
     NSTimeInterval *_dt;
     NSTimeInterval *_lastUpdateTime;
+    double speed;
+    bool touch;
 }
 
 -(id)initWithSize:(CGSize)size{
+    speed = 1;
     if(self = [super initWithSize:size]){
-        //[self initalizingScrollingBackground];
-        //[self train];
         self.backgroundColor = [SKColor colorWithRed:.15 green:.15 blue:.3 alpha:1];
-        
         _bgLayer = [SKNode node];
         [self addChild: _bgLayer];
-        
         _gameLayer = [SKNode node];
         [self addChild: _gameLayer];
-        
         _HUDLayer = [SKNode node];
         [self addChild: _HUDLayer];
         
-        SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"Sky.png"];
-        SKAction *moveBg= [SKAction moveByX:-backgroundTexture.size.width*2 y:0 duration: 0.1*backgroundTexture.size.width];    //moves background
-        SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width*2 y:0 duration:0];   //reset background
-        SKAction *moveBackgroundForever = [SKAction repeatActionForever:[SKAction sequence:@[moveBg, resetBg]]];    //repeat moveBg and resetBg
-        
-        for(int i =0; i<2+self.frame.size.width/(backgroundTexture.size.width*2); i++){     //create sprite to call moveBgForever
-            SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
-            [sprite setScale:1];
-            sprite.zPosition=-20;
-            sprite.anchorPoint=CGPointZero;
-            sprite.position=CGPointMake(i*sprite.size.width, 0);
-            [sprite runAction:moveBackgroundForever];
-            [_bgLayer addChild:sprite];
-        }
-        
-        
+        [self initScrollingBackground]; //scolling background (buildings, hills, etc.)
+        [self initScrollingForeground]; //scolling tracks
+        [self train];   //train object with physics body
+
     }
     return self;
-
 }
-/*
--(void)train{
-    _train = [SKNode node];
-    SKShapeNode *rect = [SKShapeNode node];
+
+-(void)initScrollingForeground{ //Scrolling tracks function
+    SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"Runway.png"]; //change runway to train tracks
+    SKAction *moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:.02*speed*groundTexture.size.width*2];
+    SKAction *resetGroundSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0];
+    SKAction *moveGroundSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
+    
+    for(int i=0; i<2 +self.frame.size.width/(groundTexture.size.width*2);i++){      //place image
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
+        [sprite setScale:1];
+        sprite.zPosition = 10;
+        sprite.anchorPoint = CGPointZero;
+        sprite.position = CGPointMake(i*sprite.size.width, -100);
+        [sprite runAction:moveGroundSpriteForever];
+        [_gameLayer addChild:sprite];
+    }
+}
+-(void)initScrollingBackground{   //scrolling background function
+    SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"Sky.png"];        //reuse sky image
+    SKAction *moveBg= [SKAction moveByX:-backgroundTexture.size.width*2 y:0 duration: 0.1*speed*backgroundTexture.size.width]; //move Bg
+    SKAction *resetBg = [SKAction moveByX:backgroundTexture.size.width*2 y:0 duration:0];   //reset background
+    SKAction *moveBackgroundForever = [SKAction repeatActionForever:[SKAction sequence:@[moveBg, resetBg]]];    //repeat moveBg and resetBg
+    for(int i =0; i<2+self.frame.size.width/(backgroundTexture.size.width*2); i++){     //place image
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:backgroundTexture];
+        [sprite setScale:1];
+        sprite.zPosition=-20;
+        sprite.anchorPoint=CGPointZero;
+        sprite.position=CGPointMake(i*sprite.size.width, 100);
+        [sprite runAction:moveBackgroundForever];
+        [_bgLayer addChild:sprite];
+    }
+}
+
+
+-(void)train{   //Moving object
+    _train = [SKSpriteNode spriteNodeWithImageNamed:@"AirplaneCartoon.png"];//change to train png
+    _train.position = CGPointMake(60, 50);
+    _train.zPosition = 50;
+    _train.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(46, 18)];
+    _train.physicsBody.dynamic = YES;
+    _train.physicsBody.affectedByGravity = NO;
+    _train.physicsBody.allowsRotation = NO;
+    [_gameLayer addChild:_train];
+    
+    
+    /*SKShapeNode *rect = [SKShapeNode node];
     rect.path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(-10, 10, 20, 20)].CGPath;
     rect.fillColor = [UIColor blueColor];
     rect.strokeColor = [UIColor blueColor];
@@ -64,10 +91,16 @@
     _train.position = CGPointMake(screenWidth/2, screenHeight/2);
     
     
-    [self addChild:_train];
+    [self addChild:_train];*/
 }
-*/
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    speed = speed/2;
+    [self initScrollingBackground]; //fix speed increased for scrolling
+    [self initScrollingForeground];
+    
+    
     /* Called when a touch begins */
     /*
     for (UITouch *touch in touches) {
