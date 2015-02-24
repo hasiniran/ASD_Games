ab2bdfbb01d117dca6bd0011a93da83a66959bcd
mperez14 <mperez14@nd.edu>
2015-02-24 00:51:43 -0500

    Synced Level 1 and 3 Level 3 has motion, stops at station, then displays text asking question. Need: implement characters

--- Level1.h
+++ Level1.h
@@ -8,12 +8,13 @@
 //#ifndef ASD_Game_Level1_h
 //#define ASD_Game_Level1_h
 #import <SpriteKit/SpriteKit.h>
-//#import "Level2.h"
+#import "Level3.h"
 
 
 //LEVEL1
 @interface Level1 : SKScene {
 
+    
 }
 
 
