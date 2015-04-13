6e54d0086978dc4e4f55def73836fc6599f5d6f8
Charles Shinaver <bshinaver@gmail.com>
2015-04-12 23:55:05 -0400

    Return stash

--- StartUpMenu.m
+++ StartUpMenu.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "StartupMenu.h"
+#import "SecondLevel.h"
 
 
 
@@ -20,61 +21,70 @@ SKScene * scene;
     
     if (self = [super initWithSize:size]) {
 
-        SKLabelNode *Game1 = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        Game1.text = @"Airplane Game"; //Set the button text
-        Game1.name = @"Airplane";
-        Game1.fontSize = 40;
-        Game1.fontColor = [SKColor yellowColor];
-        Game1.position = CGPointMake(self.size.width/2, self.size.height/2);
+        //SKLabelNode *Game1 = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        //Game1.text = @"Airplane Game"; //Set the button text
+        //Game1.name = @"Airplane";
+        //Game1.fontSize = 40;
+        //Game1.fontColor = [SKColor yellowColor];
+        //Game1.position = CGPointMake(self.size.width/2, self.size.height/2);
       
+        //
+        //SKLabelNode *Game2 = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];   //Second button goes to train game
+        //Game2.text = @"Train Game";
+        //Game2.name = @"Train";
+        //Game2.fontSize = 40;
+        //Game2.fontColor = [SKColor redColor];
+        //Game2.position = CGPointMake(self.size.width/2, self.size.height/4);    //Put train button underneath, airplane button
+        //
+        //[self addChild:Game1];
+        //[self addChild:Game2];
         
-        SKLabelNode *Game2 = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];   //Second button goes to train game
-        Game2.text = @"Train Game";
-        Game2.name = @"Train";
-        Game2.fontSize = 40;
-        Game2.fontColor = [SKColor redColor];
-        Game2.position = CGPointMake(self.size.width/2, self.size.height/4);    //Put train button underneath, airplane button
-        
-        [self addChild:Game1];
-        [self addChild:Game2];
     }
     
     return self;
 }
 
 
-
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    UITouch *touch = [touches anyObject];
-    CGPoint location = [touch locationInNode:self];
-    SKNode *node = [self nodeAtPoint:location];
-    
-    SKNode *trainNode = [self nodeAtPoint:location];
+    //UITouch *touch = [touches anyObject];
+    //CGPoint location = [touch locationInNode:self];
+    //SKNode *node = [self nodeAtPoint:location];
+    //
+    //SKNode *trainNode = [self nodeAtPoint:location];
     
     //launch the first scene of the airplane game if the Airplane button is touched
    
-    if ([node.name isEqualToString:@"Airplane"]) {
-        
-        SKTransition *reveal = [SKTransition doorsOpenHorizontalWithDuration :1.0];
-        
-        AirplaneScene1 * scene = [AirplaneScene1 sceneWithSize:self.view.bounds.size];    //commented out in order to test Train game
-        scene.scaleMode = SKSceneScaleModeAspectFill;
-        [self.view presentScene:scene transition: reveal];
-        
-    }
+    /* For demo */
+    SKTransition *reveal = [SKTransition doorsOpenHorizontalWithDuration :1.0];
     
+    SecondLevel *scene = [SecondLevel sceneWithSize:self.view.bounds.size];
+    //AirplaneScene1 * scene = [AirplaneScene1 sceneWithSize:self.view.bounds.size];    //commented out in order to test Train game
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    [self.view presentScene:scene transition: reveal];
     
-    //if train button is pressed, Go to train game
-    if ([trainNode.name isEqualToString:@"Train"]) {
-        
-        SKTransition *reveal = [SKTransition doorsOpenHorizontalWithDuration :1.0];
-        Level5 *scene= [Level5 sceneWithSize:self.view.bounds.size];
-        
-        scene.scaleMode = SKSceneScaleModeAspectFill;
-        [self.view presentScene:scene transition: reveal];
-    }
+    /* ######## */
+    //if ([node.name isEqualToString:@"Airplane"]) {
+    //    
+    //    SKTransition *reveal = [SKTransition doorsOpenHorizontalWithDuration :1.0];
+    //    
+    //    AirplaneScene1 * scene = [AirplaneScene1 sceneWithSize:self.view.bounds.size];    //commented out in order to test Train game
+    //    scene.scaleMode = SKSceneScaleModeAspectFill;
+    //    [self.view presentScene:scene transition: reveal];
+    //    
+    //}
+    //
+    //
+    ////if train button is pressed, Go to train game
+    //if ([trainNode.name isEqualToString:@"Train"]) {
+    //    
+    //    SKTransition *reveal = [SKTransition doorsOpenHorizontalWithDuration :1.0];
+    //    Level5 *scene= [Level5 sceneWithSize:self.view.bounds.size];
+    //    
+    //    scene.scaleMode = SKSceneScaleModeAspectFill;
+    //    [self.view presentScene:scene transition: reveal];
+    //}
     
     
 }
